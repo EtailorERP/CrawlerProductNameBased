73fd91390874848b6db229603c3b41dff02cc5b0
KlwntSingh <android.developercode@gmail.com>
2015-07-01 12:27:08 +0530

    improved

--- client/scripts/services/parser.js
+++ client/scripts/services/parser.js
@@ -3,7 +3,7 @@ angular.module('priceFetcher').service('parser',function(){
 	this.checkEmpty = function(data){
 		var status = true;
 		console.log(data);
-		if(data == undefined)
+		if(data == undefined || data == '')
 			status = false;
 		
 		return status;
